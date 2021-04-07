e72b909e98b3ab5e337d6650644c1c33d76063ff
BuildTools <unconfigured@null.spigotmc.org>
2021-04-08 00:36:51 +0900

    Masterにコミットしたので取り消し

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ intents.members = True
 with open("token", "r") as f:  # Token goes in file "token"
     TOKEN = f.read()
 
-
 def when_mentioned_or_function(func):
     def inner(bot, message):
         r = func(bot, message)
         prefixes = commands.when_mentioned(bot, message)
         prefixes.append(r)
         return prefixes
-
     return inner
 
 
@@ -29,6 +27,7 @@ bot.handler = AntiSpamHandler(bot, no_punish=True)
 bot.tracker = AntiSpamTracker(bot.handler, 3)
 bot.handler.register_extension(bot.tracker)
 
+
 # Cogs
 
 bot.add_cog(Errors.ErrorCog(bot))
@@ -38,3 +37,4 @@ bot.add_cog(User.UserCommandsCog(bot))
 bot.add_cog(Events.EventsCog(bot))
 
 bot.run(TOKEN)
+
